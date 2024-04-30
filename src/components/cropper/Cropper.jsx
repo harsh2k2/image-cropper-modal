342c38be50ae24cbd6fa260628416aab5230c5f2
Harsh Kumar <72307191+harsh2k2@users.noreply.github.com>
2024-04-30 17:29:20 +0530

    image handling
    handled case where image is large/ out of the box

--- src/components/cropper/Cropper.jsx
+++ src/components/cropper/Cropper.jsx
@@ -1,9 +1,17 @@
-import EasyCropper from 'react-easy-crop';
-import { useImageCropContext } from '@/providers/ImageCropProvider';
+import EasyCropper from "react-easy-crop";
+import { useImageCropContext } from "@/providers/ImageCropProvider";
 
 const Cropper = () => {
-  const { image, zoom, setZoom, rotation, setRotation, crop, setCrop, onCropComplete } =
-    useImageCropContext();
+  const {
+    image,
+    zoom,
+    setZoom,
+    rotation,
+    setRotation,
+    crop,
+    setCrop,
+    onCropComplete,
+  } = useImageCropContext();
 
   return (
     <EasyCropper
@@ -18,17 +26,17 @@ const Cropper = () => {
       onZoomChange={setZoom}
       setRotation={setRotation}
       showGrid={false}
-      cropSize={{ width: 185, height: 185 }}
-      style={{
-        containerStyle: {
-          height: 220,
-          width: 220,
-          top: 8,
-          bottom: 8,
-          left: 8,
-          right: 8
-        }
-      }}
+      // cropSize={{ width: 185, height: 185 }}
+      // style={{
+      //   containerStyle: {
+      //     height: 220,
+      //     width: 220,
+      //     top: 8,
+      //     bottom: 8,
+      //     left: 8,
+      //     right: 8,
+      //   },
+      // }}
     />
   );
 };
