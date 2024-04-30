937e3d42303000d443630c252452849b61c2093d
Harsh Kumar <72307191+harsh2k2@users.noreply.github.com>
2024-04-30 10:16:49 +0530

    react-hot-toast

--- src/components/ImageCrop.jsx
+++ src/components/ImageCrop.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import user1 from '@/assets/user_1.png';
-import Modal from '@/components/base/Modal';
-import { readFile } from '@/helpers/cropImage';
-import ImageCropModalContent from './ImageCropModalContent';
-import { useImageCropContext } from '@/providers/ImageCropProvider';
+import { useState } from "react";
+import user1 from "@/assets/user_1.png";
+import Modal from "@/components/base/Modal";
+import { readFile } from "@/helpers/cropImage";
+import ImageCropModalContent from "./ImageCropModalContent";
+import { useImageCropContext } from "@/providers/ImageCropProvider";
+import toast, { Toaster } from "react-hot-toast";
 
 const ImageCrop = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -20,34 +21,48 @@ const ImageCrop = () => {
 
   const handleFileChange = async ({ target: { files } }) => {
     const file = files && files[0];
+
+    if (file && file.size > 1024 * 1024) {
+      // alert("Please select an image of size less than 1MB");
+
+      toast.error("Please select an image of size less than 1MB");
+      return;
+    }
+
     const imageDataUrl = await readFile(file);
     setImage(imageDataUrl);
     setOpenModal(true);
   };
 
   return (
-    <div className="bg-gray-100 h-screen flex justify-center items-center">
-      <input
-        type="file"
-        onChange={handleFileChange}
-        className="hidden"
-        id="avatarInput"
-        accept="image/*"
-      />
-      <label htmlFor="avatarInput" className="cursor-pointer">
-        <img
-          src={preview}
-          height={192}
-          width={192}
-          className="object-cover rounded-full h-48 w-48"
-          alt=""
+    <>
+      <Toaster />
+      <div className="bg-gray-100 h-screen flex justify-center items-center">
+        <input
+          type="file"
+          onChange={handleFileChange}
+          className="hidden"
+          id="avatarInput"
+          accept="image/*"
         />
-      </label>
+        <label htmlFor="avatarInput" className="cursor-pointer">
+          <img
+            src={preview}
+            height={192}
+            width={192}
+            className="object-cover rounded-full h-48 w-48"
+            alt=""
+          />
+        </label>
 
-      <Modal open={openModal} handleClose={() => setOpenModal(false)}>
-        <ImageCropModalContent handleDone={handleDone} handleClose={() => setOpenModal(false)} />
-      </Modal>
-    </div>
+        <Modal open={openModal} handleClose={() => setOpenModal(false)}>
+          <ImageCropModalContent
+            handleDone={handleDone}
+            handleClose={() => setOpenModal(false)}
+          />
+        </Modal>
+      </div>
+    </>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'image-cropper-modal',
+  // base: 'image-cropper-modal',
   plugins: [tsconfigPaths(), react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
